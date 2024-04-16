4389f492d15bb73e93e0f8d1c7006bc410dbd7b5
rabieHs <rabiehoussaini@gmail.com>
2024-04-16 08:32:59 +0100

    add search buss

--- lib/models/bus.dart
+++ lib/models/bus.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Bus {
   final String busId;
@@ -20,4 +22,24 @@ class BusRoute {
   final DateTime arrivalTime;
 
   BusRoute({required this.cityName, required this.arrivalTime});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'cityName': cityName,
+      'arrivalTime': arrivalTime.millisecondsSinceEpoch,
+    };
+  }
+
+  factory BusRoute.fromMap(Map<String, dynamic> map) {
+    return BusRoute(
+      cityName: map['cityName'] as String,
+      arrivalTime:
+          DateTime.fromMillisecondsSinceEpoch(map['arrivalTime'] as int),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory BusRoute.fromJson(String source) =>
+      BusRoute.fromMap(json.decode(source) as Map<String, dynamic>);
 }
